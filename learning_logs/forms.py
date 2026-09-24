7c790bb44e603c99b27e22ff0f563a291f84f73e
sjbsr <sjbsr@outlook.com>
2026-09-24 18:21:03 -0400

    debug 'learning_logs/forms.py'

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -8,7 +8,7 @@ class TopicForm(forms.ModelForm):
         fields = ['text']
         labels = {'text': ''}
 
-class EntryForm(forms.modelForm):
+class EntryForm(forms.ModelForm):
     class Meta:
             model = Topic
             fields = ['text']
